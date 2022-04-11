f6bc9f9c43ac48b151739d4bedf7afd9264c4baf
sabrsabrsabrsabr <wimarof479@reimondo.com>
2022-04-12 02:26:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector("body").style.backgroundColor = "green"
+var matn = prompt("Matn kiriting.");
+var paragraph = document.createElement("uyishi")
+paragraph.textContent = matn;
+document.body.appendChild(paragraph)
